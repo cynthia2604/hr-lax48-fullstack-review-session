c8c3e73fcbb09027665bec0feea93f6dd42644d3
“Cynthia <xin2604@gmail.com>
2021-12-18 09:50:57 -0800

    refactor ListElement and add toggle for name changing

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ListElement from './ListElement.jsx';
 
-const List = (props) =>
-  <div>
-    {props.students.map((student) =>
-      <ListElement student={student} key={student._id}/>
-    )}
-  </div>
-
+const List = (props) => (
+    <div>
+      {props.students.map((student) =>
+        <ListElement student={student} key={student._id}/>
+      )}
+    </div>
+)
 
 export default List
--- client/src/components/ListElement.jsx
+++ client/src/components/ListElement.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
-const ListElement = (props) =>
-  <span>
-    <div>{props.student.name}</div>
-    <img src={props.student.imageUrl}></img>
-  </span>
-
-
+class ListElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleEdit : false
+    }
+    this.editMode = this.editMode.bind(this);
+    this.changeNameMode = this.changeNameMode.bind(this);
+  }
+  // changeName // track of input change name
+  changeNameMode() {
+    if (this.state.toggleEdit) {
+      return (
+        <div>
+          <input type="text" name="name" placeholder={this.props.student.name}></input>
+          <button>Cancel</button>
+          <button>Update</button>
+        </div>
+      )
+    }
+  }
+  // hide or disappear changename component
+  editMode() {
+    this.setState({
+      toggleEdit : !this.state.toggleEdit
+    })
+  }
+  render() {
+    return (
+    <span>
+      <div onClick={this.editMode}>{this.props.student.name}</div>
+      {this.changeNameMode()}
+      <img src={this.props.student.imageUrl}></img>
+    </span>
+    )
+  }
+}
 export default ListElement
